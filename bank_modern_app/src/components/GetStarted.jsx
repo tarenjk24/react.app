6d5fe8c0fc5bb07177512adecb554c61b3bf1b82
Eter Nada <123267672+tarenjk24@users.noreply.github.com>
2024-07-23 16:16:59 +0000

    update

--- bank_modern_app/src/components/GetStarted.jsx
+++ bank_modern_app/src/components/GetStarted.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import { styles } from "../style";
+import { arrowUp } from "../assets";
 
-const App = () => (
-  <div className="bg-primary w-full overflow-hidden">
-    <h1>Hello, world</h1>
+const GetStarted = () => (
+  <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+    <div className={`${styles.flexCenter} flex-col bg-primary w-[100px] h-[100px] rounded-full`}>
+      <div className={`${styles.flexStart} flex-row`}>
+        <p className="font-poppins font-medium text-[18px] leading-[23px]">
+          <span className="text-gradient">Get</span>
+          <img src={arrowUp} alt="arrow" className="w-[23px] h-[23px] object-contain" />
+        </p>
+      </div>
+      <p className="font-poppins font-medium text-[18px] leading-[23px]">
+          <span className="text-gradient">Started</span>
+          <img src={arrowUp} alt="arrow" className="w-[23px] h-[23px] object-contain" />
+        </p>
+    </div>
   </div>
 );
 
-export default App;
+export default GetStarted;
